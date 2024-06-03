e0d69097b8f5774559fb7334cf1f073f754fb58f
aknonr <133671359+aknonr@users.noreply.github.com>
2024-06-03 22:17:41 +0300

    Entites product ve catalog eklenildi

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // Benzersiz olduğunu bildirmek için bunun bir Id Olduğunu bildirmiş olduk.
+
+        public string CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+
+    }
+}
