5ee8ce1e1ff92896a700a8b774f65b32cacc6de8
Akın <aknonr23@hotmail.com>
2024-07-19 17:28:54 +0300

    Product Service oluşturuldu ve servisler yazıldı

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductServices/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.Catalog.Dtos.ProductDtos;
+
+namespace MultiShop.Catalog.Services.ProductServices
+{
+    public interface IProductService
+    { 
+        //Product ile ilgili crud metotlarını tutacağımız sınıftır
+
+        Task<List<ResultProductDto>> GetAllProductAsync(); //Bütün veriliermizi getirecek metot
+
+        Task CreateProductAsync(CreateProductDto createProductDto); //Ekleme işlemi 
+
+        Task UpdateProductAsync(UpdateProductDto updateProductDto); //Güncelleme islemi yapacak metot
+
+        Task DeleteProductAsync(string id);                           //Silme islemi yapacak metot
+
+        Task<GetByIdProductDto> GetByIdProductAsync(string id);        //Id 'e göre getirme islemi
+    }
+}
