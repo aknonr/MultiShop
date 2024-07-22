31c166e98193e7ce76191a2e27b5086d2882a2bf
Akın <aknonr23@hotmail.com>
2024-07-22 17:22:28 +0300

    Product ımage ve detail controller apı eklenildi

---
+++ Services/Catalog/MultiShop.Catalog/Controller/ProductDetailsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDetailDto;
+using MultiShop.Catalog.Services.ProductDetailDetailServices;
+
+
+namespace MultiShop.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _ProductDetailsService;
+
+        public ProductDetailsController(IProductDetailService ProductDetailsService)
+        {
+            _ProductDetailsService = ProductDetailsService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ProductDetailsList()
+        {
+            var values = await _ProductDetailsService.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> CreateProductDetails(CreateProductDetailDto createProductDetailsDto)
+        {
+            // Mapleme yapmasaydık bu nesne örneği üzerinde her birine proeperty atama yapmamiz  gerekiyordu Asagıdaki gibi atama yapacaktık
+            //ProductDetails ProductDetails =new ProductDetails();
+            //ProductDetails.ProductDetailsName=createProductDetailsDto.ProductDetailsName;
+
+            await _ProductDetailsService.CreateProductDetailAsync(createProductDetailsDto);
+            return Ok("Ürün detayı başarıyla eklendi");
+
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCtegory(string id)
+        {
+            await _ProductDetailsService.DeleteProductDetailAsync(id);
+            return Ok("\"Ürün detayı başarıyla silindi");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateProductDetails(UpdateProductDetailDto updateProductDetailsDto)
+        {
+            await _ProductDetailsService.UpdateProductDetailAsync(updateProductDetailsDto);
+            return Ok("Ürün detayı başarıyla güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Controller/ProductImagesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            // Mapleme yapmasaydık bu nesne örneği üzerinde her birine proeperty atama yapmamiz  gerekiyordu Asagıdaki gibi atama yapacaktık
+            //ProductImage ProductImage =new ProductImage();
+            //ProductImage.ProductImageName=createProductImageDto.ProductImageName;
+
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün görselleri başarıyla eklendi");
+
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCtegory(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün görselleri başarıyla silindi");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün görselleri başarıyla güncellendi");
+        }
+    }
+}
