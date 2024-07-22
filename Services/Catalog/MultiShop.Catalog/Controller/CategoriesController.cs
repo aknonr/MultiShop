65ed7e6c6ede6a0f5a5ebb9e5a5767319ae79a67
Akın <aknonr23@hotmail.com>
2024-07-22 17:16:15 +0300

    Product controller ve Kategori controller eklenildi

---
+++ Services/Catalog/MultiShop.Catalog/Controller/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService; 
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var values =await _categoryService.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            // Mapleme yapmasaydık bu nesne örneği üzerinde her birine proeperty atama yapmamiz  gerekiyordu Asagıdaki gibi atama yapacaktık
+            //Category category =new Category();
+            //category.CategoryName=createCategoryDto.CategoryName;
+
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori başarıyla eklendi");
+
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCtegory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori başarıyla silindi");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await  _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori başarıyla güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Controller/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductServices;
+
+namespace MultiShop.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _ProductService;
+
+        public ProductController(IProductService ProductService)
+        {
+            _ProductService = ProductService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _ProductService.GetAllProductAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            // Mapleme yapmasaydık bu nesne örneği üzerinde her birine proeperty atama yapmamiz  gerekiyordu Asagıdaki gibi atama yapacaktık
+            //Product Product =new Product();
+            //Product.ProductName=createProductDto.ProductName;
+
+            await _ProductService.CreateProductAsync(createProductDto);
+            return Ok("Ürün başarıyla eklendi");
+
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCtegory(string id)
+        {
+            await _ProductService.DeleteProductAsync(id);
+            return Ok("Ürün başarıyla silindi");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _ProductService.UpdateProductAsync(updateProductDto);
+            return Ok("Ürün başarıyla güncellendi");
+        }
+    }
+}
