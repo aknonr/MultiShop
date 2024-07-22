f759dfd2156a2662dfc657822e210ed0242c4046
Akın <aknonr23@hotmail.com>
2024-07-22 17:33:05 +0300

    program.cs de AutoMapper kullanıldı

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,6 +1,19 @@
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.ProductDetailDetailServices;
+using MultiShop.Catalog.Services.ProductImageServices;
+using MultiShop.Catalog.Services.ProductServices;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Constructor için konfigrasyonu yapt???m?z yer
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+builder.Services.AddScoped<IProductService,ProductService>();
+builder.Services.AddScoped<IProductDetailService,ProductDetailDetailService>();
+builder.Services.AddScoped<IProductImageService,ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
