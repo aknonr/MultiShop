0e04ee89427b0b642d0bc6c1cca8e29166de5f0c
Akın <aknonr23@hotmail.com>
2024-07-24 17:45:26 +0300

    Discoun created

---
+++ MultiShop/Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Tablomuzu veri tabanina yansitmak icin islemleri ve yol buradan yapacagız
+
+            optionsBuilder.UseSqlServer("Server=RETMIND; initial Catalog=MultiShopDiscountDb; integrated Security=true");
+           
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
