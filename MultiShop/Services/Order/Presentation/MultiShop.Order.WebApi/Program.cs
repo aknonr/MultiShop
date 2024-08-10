27fb8d66f0631cd2ab5d673c2365e23faedc6037
Akın <aknonr23@hotmail.com>
2024-08-10 16:35:19 +0300

    edit discount controller port number and Autohuration controller

--- MultiShop/Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ MultiShop/Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,5 @@
-﻿using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
 using MultiShop.Order.Application.Interfaces;
 using MultiShop.Order.Application.Services;
@@ -6,6 +7,15 @@ using MultiShop.Order.Persistance.Context;
 using MultiShop.Order.Persistance.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceOrder";
+    opt.RequireHttpsMetadata = false;
+});
+
 
 builder.Services.AddDbContext<OrderContext>();
 
@@ -48,6 +58,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
