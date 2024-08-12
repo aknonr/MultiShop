ea21e2c965bbe688a29806e5554dc8cc4a335b14
Akın <aknonr23@hotmail.com>
2024-08-12 15:12:10 +0300

    add repository design pattern using for data acces layer

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.DataAccesLayer/Abstract/ICargoCompanyDal.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccesLayer.Abstract
+{
+    public interface ICargoCompanyDal:IGenericDal<CargoCompany>
+    {
+       
+
+
+    }
+}
---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.DataAccesLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccesLayer.Abstract
+{
+
+    //Repository Design Pattern Generic olarak yaptık
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+
+        //T türünde id getirelecek
+        T GetById(int id);
+
+        //  Bütün veriyi liste halinde getirecek Metod
+        List<T> GetAll();
+
+
+    }
+}
